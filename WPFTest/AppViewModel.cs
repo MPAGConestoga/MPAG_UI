ce82281d84da94d7d94177d0e5989ba5eaa65395
Paul Smith <Psmith4422@conestogac.on.ca>
2019-11-21 14:13:03 -0500

    Add project files.

---
+++ WPFTest/AppViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFTest
+{
+   public class AppViewModel : ObservableObject
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { OnPropertyChanged(ref _currentView, value);  }
+        }
+
+        private ViewModels.OrderViewModel _orderVM;
+        public ViewModels.OrderViewModel OrderVM
+        {
+            get { return _orderVM; }
+            set { OnPropertyChanged(ref _orderVM, value); }
+        }
+
+        public AppViewModel()
+        {
+            OrderVM = new ViewModels.OrderViewModel();
+            CurrentView = OrderVM;
+        }
+    }
+}
---
+++ WPFTest/Models/PendingOrders.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFTest.Models
+{
+    public class PendingOrders : ObservableObject
+    {
+        private int orderID;
+        public int OrderID
+        {
+            get { return orderID; }
+            set { OnPropertyChanged(ref orderID, value);  }
+        }
+
+        private string customer;
+        public string Customer
+        {
+            get { return customer; }
+            set { OnPropertyChanged(ref customer, value); }
+        }
+
+        private JobType jobType;
+        public JobType JobType
+        {
+            get { return jobType; }
+            set { OnPropertyChanged(ref jobType, value); }
+        }
+
+        private string origin;
+        public string Origin
+        {
+            get { return origin; }
+            set { OnPropertyChanged(ref origin, value); }
+        }
+
+        private string destination;
+        public string Destination
+        {
+            get { return destination; }
+            set { OnPropertyChanged(ref destination, value); }
+        }
+
+        private VanType vanType;
+        public VanType VanType
+        {
+            get { return vanType; }
+            set { OnPropertyChanged(ref vanType, value); }
+        }
+
+        private int attachedCarriers;
+        public int AttachedCarriers
+        {
+            get { return attachedCarriers; }
+            set { OnPropertyChanged(ref attachedCarriers, value); }
+        }
+
+        public static ObservableCollection<PendingOrders> GetPending()
+        {
+            var pendingColl = new ObservableCollection<PendingOrders>();
+            pendingColl.Add(new PendingOrders() { OrderID = 001, Customer = "Jev Robotics", JobType = JobType.FTL, Origin = "London", Destination = "Toronto", VanType = VanType.Dry, AttachedCarriers = 0 });
+            pendingColl.Add(new PendingOrders() { OrderID = 002, Customer = "Ishmail's Fish", JobType = JobType.LTL, Origin = "Windsor", Destination = "London", VanType = VanType.Reefer, AttachedCarriers = 0 });
+            pendingColl.Add(new PendingOrders() { OrderID = 003, Customer = "Cats", JobType = JobType.FTL, Origin = "Toronto", Destination = "Kingston", VanType = VanType.Dry, AttachedCarriers = 0 });
+            pendingColl.Add(new PendingOrders() { OrderID = 004, Customer = "Ice Sculptors United", JobType = JobType.LTL, Origin = "Belleville", Destination = "Ottawa", VanType = VanType.Dry, AttachedCarriers = 0 });
+            pendingColl.Add(new PendingOrders() { OrderID = 005, Customer = "Factory Shoe", JobType = JobType.FTL, Origin = "Kingston", Destination = "Hamilton", VanType = VanType.Dry, AttachedCarriers = 0 });
+
+            return pendingColl;
+        }
+    }
+
+    public enum VanType
+    {
+        Dry,
+        Reefer
+    }
+
+    public enum JobType
+    {
+        FTL,
+        LTL
+    }
+}
